6271a9ff1048a58c19398ff34fde3dc3105702d7
Dedy Yugo Purwanto <dedy.yugo.purwanto@gmail.com>
2018-05-31 13:38:07 +0700

    Initial commit

---
+++ app/Http/Controllers/DashboardController.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\View\View;
+
+class DashboardController extends Controller
+{
+    public function __invoke(): View
+    {
+        return view('dashboard')
+            ->withTitle(__('Dashboard'));
+    }
+}
---
+++ routes/web.php
@@ -0,0 +1,31 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
+
+Route::get('dashboard', 'DashboardController')->name('dashboard');
+
+Route::group(['prefix' => 'pages', 'as' => 'pages.'], function(){
+    Route::view('general', 'pages.general', ['title' => __('General')])->name('general');
+    Route::view('button', 'pages.button', ['title' => __('Buttons')])->name('button');
+    Route::view('form', 'pages.form', ['title' => __('Forms')])->name('form');
+    Route::view('table', 'pages.table', ['title' => __('Tables')])->name('table');
+    Route::view('icon', 'pages.icon', ['title' => __('Icons')])->name('icon');
+    Route::view('slider', 'pages.slider', ['title' => __('Sliders')])->name('slider');
+});
