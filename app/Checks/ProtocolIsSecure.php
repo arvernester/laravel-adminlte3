61b23bab4d1412e3160951526f758e0642b003e5
Dedy Yugo Purwanto <dedy.yugo.purwanto@gmail.com>
2018-07-08 08:47:06 +0700

    Check protocol must be http in production

---
+++ app/Checks/ProtocolIsSecure.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Checks;
+
+use BeyondCode\SelfDiagnosis\Checks\Check;
+
+class ProtocolIsSecure implements Check
+{
+    /**
+     * The name of the check.
+     *
+     * @return string
+     */
+    public function name(): string
+    {
+        return 'The protocol must be secured';
+    }
+
+    /**
+     * Perform the actual verification of this check.
+     *
+     * @return boolean
+     */
+    public function check(): bool
+    {
+        return str_contains('https', env('APP_URL'));
+    }
+
+    /**
+     * The error message to display in case the check does not pass.
+     *
+     * @return string
+     */
+    public function message(): string
+    {
+        return 'Please activate SSL to enable secured protocol.';
+    }
+}
