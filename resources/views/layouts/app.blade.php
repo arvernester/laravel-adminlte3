6c78e51ef3efd4ec91d4c2aa59d4fbcb12889c1e
Yugo <dedy.yugo.purwanto@gmail.com>
2019-05-18 06:09:47 +0700

    Fixed unwrapped CSS

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -20,7 +20,9 @@
   <link rel="stylesheet" href="{{ asset('css/adminlte.min.css') }}">
 
   <!-- Google Font: Source Sans Pro -->
-  <link href="//fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700" rel="stylesheet"> @stack('css')
+  <link href="//fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700" rel="stylesheet">
+
+  @stack('css')
 </head>
 
 <body class="hold-transition sidebar-mini">
